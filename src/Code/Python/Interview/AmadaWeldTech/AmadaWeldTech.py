958e0c19fe75bc255307e6f987876c562593266b
JasenHansen <jasenhansen@yahoo.com>
2023-04-17 20:50:53 -0700

    Work added and also a code challenge

--- src/Code/Python/Interview/AmadaWeldTech/AmadaWeldTech.py
+++ src/Code/Python/Interview/AmadaWeldTech/AmadaWeldTech.py
@@ -1,53 +1,104 @@
 #!/usr/bin/env python3
+
+# noinspection SpellCheckingInspection,PyTypeChecker
+import inspect
 import os
 
 
-# noinspection SpellCheckingInspection,PyTypeChecker
-class Chalenge(object):
-    """Epiris main class."""
+class AmadaWeldTech(object):
+    """AmadaWeldTech main class."""
 
     @staticmethod
     def get_description_alphabetical():
         print("""
               Coding challenge:
                  1. read in text file.
-                 2. Reverse characters in line.
-                 3. Print to output file.""")
+                 2. Reverse characters in lines.
+                 3. Print to output file.
+              Bonus 1:
+                 1. read in text file.
+                 2. Reverse lines in file.
+                 3. Print to output file.
+              Bonus 2:
+                 1. read in text file.
+                 2. Reverse lines in file.
+                 3. Reverse characters in lines.
+                 4. Print to output file.""")
 
-    def __init__(self,
-                 input_file="C:\\Users\\jasen\\IdeaProjects\\portfolio\\src\\Code\\Python\\Interview\\AmadaWeldTech\\data\\data.txt"):
-        self.input_file = input_file
+    def __init__(self, input_file=None):
+        if not input_file:
+            # Dynamically generate a file to ./data/data.txt as a default file
+            input_file = os.path.dirname(os.path.abspath(__file__))
+            input_file = os.path.join(input_file, "data")
+            self.input_file = os.path.join(input_file, "data.txt")
+        else:
+            self.input_file = os.path.abspath(input_file)
+        # If the data file does not exist, create one
+        if not os.path.exists(self.input_file):
+            # Generate output file
+            output = open(self.input_file, 'w+')
+            output.write(f"File did not exist!!!\n")
+            output.write(f"Making default data.\n")
+            output.close()
+        # Opening file to get the data
         self.lines = None
+        with open(self.input_file) as file:
+            self.lines = [line.rstrip() for line in file]
 
-    def read_file(self):
-        # Using readlines()
-        self.lines = open(self.input_file, 'r')
+    def reverse_lines(self):
+        # Generate output file name
+        output_file = self.generate_output_name(method=inspect.stack()[0][3])
+        # reverse order of lines
+        self.reverse_line_order()
+        # Generate output file
+        self.generate_output_file(output_file)
 
-    def reverse_lines(self,
-                      output_file="C:\\Users\\jasen\\IdeaProjects\\portfolio\\src\\Code\\Python\\Interview\\AmadaWeldTech\\data\\line_reverse.txt"):
+    def reverse_character(self):
+        # Generate output file name
+        output_file = self.generate_output_name(method=inspect.stack()[0][3])
+        # Edit data to reverse characters in place
+        self.reverse_character_order()
+        # Generate output file
+        self.generate_output_file(output_file)
+
+    def reverse_both(self):
+        # Generate output file name
+        output_file = self.generate_output_name(method=inspect.stack()[0][3])
+        # reverse order of lines
+        self.reverse_line_order()
+        # Edit data to reverse characters in place
+        self.reverse_character_order()
+        # Generate output file
+        self.generate_output_file(output_file)
+
+    def generate_output_name(self, method=""):
+        # Generate output file name
+        output_file = os.path.splitext(self.input_file)[0] + "_" + method + \
+                      os.path.splitext(self.input_file)[1]
+        return output_file
+
+    def generate_output_file(self, output_file):
         # Remove file if it exists
         if os.path.exists(output_file):
             os.remove(output_file)
         # Generate output file
         output = open(output_file, 'w+')
-        new_lines = []
         for line in self.lines:
-            new_lines.insert(0, line.strip())
-        for line in new_lines:
             output.write(f"{line}\n")
         output.close()
 
-    def reverse_character(self,
-                          output_file="C:\\Users\\jasen\\IdeaProjects\\portfolio\\src\\Code\\Python\\Interview\\AmadaWeldTech\\data\\character_reverse.txt"):
-        # Remove file if it exists
-        if os.path.exists(output_file):
-            os.remove(output_file)
-        # Generate output file
-        output = open(output_file, 'w+')
+    def reverse_line_order(self):
+        # reverse order of lines
+        new_lines = []
         for line in self.lines:
+            new_lines.insert(0, line)
+        self.lines = new_lines
+
+    def reverse_character_order(self):
+        # In each line, reverse order of characters
+        for index in range(len(self.lines)):
             new_line = ""
             # Reverse the characters in a line
-            for character in line.strip():
+            for character in self.lines[index]:
                 new_line = character + new_line
-            output.write(f"{new_line}\n")
-        output.close()
+            self.lines[index] = new_line
--- src/Test/Python/Interview/AmadaWeldTech/TestAmadaWeldTech.py
+++ src/Test/Python/Interview/AmadaWeldTech/TestAmadaWeldTech.py
@@ -3,7 +3,7 @@
 import unittest
 
 import src.Code.Python.Interview.AmadaWeldTech.AmadaWeldTech as baseFile
-from src.Code.Python.Interview.AmadaWeldTech.AmadaWeldTech import Chalenge
+from src.Code.Python.Interview.AmadaWeldTech.AmadaWeldTech import AmadaWeldTech
 
 
 # noinspection DuplicatedCode, SpellCheckingInspection
@@ -16,7 +16,7 @@ class TestFile(unittest.TestCase):
         """
         items_found = dir(baseFile)
         items_found = [x for x in items_found if "__" not in x]
-        _test_data = ["os", "Chalenge"]
+        _test_data = ['AmadaWeldTech', 'inspect', 'os']
         expected_not_found = [x for x in _test_data if x not in items_found]
         found_not_expected = [x for x in items_found if x not in _test_data]
         self.assertEqual(
@@ -40,27 +40,51 @@ class TestValidatePageAccess(unittest.TestCase):
     def test_init(self) -> None:
         """Assert init creates an object.
         """
-        my_class = Chalenge()
+        my_class = AmadaWeldTech()
         self.assertIsNotNone(my_class)
-
-    def test_read_file(self) -> None:
-        """Verify the input file can be read.
-        """
-        my_class = Chalenge()
-        self.assertIsNone(my_class.lines)
-        my_class.read_file()
-        self.assertIsNotNone(my_class.lines)
 
     def test_reverse_character(self) -> None:
         """Reverse characters and optput file.
         """
-        my_class = Chalenge()
-        my_class.read_file()
+        my_class = AmadaWeldTech()
         my_class.reverse_character()
 
     def test_reverse_lines(self) -> None:
         """Reverse lines and optput file.
         """
-        my_class = Chalenge()
-        my_class.read_file()
+        my_class = AmadaWeldTech()
         my_class.reverse_lines()
+
+    def test_reverse_both(self) -> None:
+        """Reverse lines and optput file.
+        """
+        my_class = AmadaWeldTech()
+        my_class.reverse_both()
+
+    def test_init_filename(self) -> None:
+        """Assert init creates an object.
+        """
+        my_class = AmadaWeldTech(
+            input_file=r'C:\Users\jasen\IdeaProjects\portfolio\src\Code\Python\Interview\AmadaWeldTech\data\data1.txt')
+        self.assertIsNotNone(my_class)
+
+    def test_reverse_character_filename(self) -> None:
+        """Reverse characters and optput file.
+        """
+        my_class = AmadaWeldTech(
+            input_file=r'C:\Users\jasen\IdeaProjects\portfolio\src\Code\Python\Interview\AmadaWeldTech\data\data1.txt')
+        my_class.reverse_character()
+
+    def test_reverse_lines_filename(self) -> None:
+        """Reverse lines and optput file.
+        """
+        my_class = AmadaWeldTech(
+            input_file=r'C:\Users\jasen\IdeaProjects\portfolio\src\Code\Python\Interview\AmadaWeldTech\data\data1.txt')
+        my_class.reverse_lines()
+
+    def test_reverse_both_filename(self) -> None:
+        """Reverse lines and optput file.
+        """
+        my_class = AmadaWeldTech(
+            input_file=r'C:\Users\jasen\IdeaProjects\portfolio\src\Code\Python\Interview\AmadaWeldTech\data\data1.txt')
+        my_class.reverse_both()
